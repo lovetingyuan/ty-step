45291ddc504baf1f237ed44101bcdc78ec30fcea
lovetingyuan <1932294867@qq.com>
2017-02-28 15:46:40 +0800

    fix test

--- karma.conf.js
+++ karma.conf.js
@@ -32,9 +32,9 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'test.js': ['babel']
-    },
+    // preprocessors: {
+    //   'test.js': ['babel']
+    // },
 
 
     // test results reporter to use
